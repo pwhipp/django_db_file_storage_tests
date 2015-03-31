25f767a3f46e9d157db25f9fca764e9020f571ab
pwhipp <paul.whipp@gmail.com>
2015-04-01 09:48:23 +1000

    Add migration

---
+++ file_uploader/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FileUploader',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('upload', models.FileField(upload_to=b'')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
