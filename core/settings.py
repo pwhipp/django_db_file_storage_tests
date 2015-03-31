cc1975cac14ad2b79909f2a94167bb84fb9d5d18
pwhipp <paul.whipp@gmail.com>
2015-04-01 09:41:46 +1000

    Add test for file uploader

--- core/settings.py
+++ core/settings.py
@@ -81,3 +81,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = '/tmp/'  # This is all just for testing
--- file_uploader/tests.py
+++ file_uploader/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.core.files.base import ContentFile
 
-# Create your tests here.
+from file_uploader.models import FileUploader
+
+
+class TestFileUploader(TestCase):
+    def test_file_uploader(self):
+        file_name = 'test.txt'
+        file_content_string = 'Test file content'
+        file_content = ContentFile(file_content_string)
+
+        uploader = FileUploader()
+        uploader.upload.save(file_name, file_content)
+        uploader.save()
+        uploader_id = uploader.id
+
+        new_uploader = FileUploader.objects.get(id=uploader_id)
+        new_uploader.upload.open('r')
+        new_uploader_file_content_string = new_uploader.upload.read()
+        new_uploader.upload.close()
+
+        self.assertEqual(file_content_string, new_uploader_file_content_string)
+
